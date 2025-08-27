a57ebee0bfe66f72b99b48a5a51dca0ef336bdf7
TolulopeBabajide <babajidetoluwalope@gmail.com>
2025-08-27 09:59:26 +0100

    revert: convert back to CommonJS for Vercel compatibility

--- apps/frontend/api/[...path].js
+++ apps/frontend/api/[...path].js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { path } = req.query;
   const backendUrl = 'https://awade-backend-test.onrender.com';
   
